7e8d929d90234df46edfe5e72307a35c54d62df2
vadivelczepak <114664327+vadivelczepak@users.noreply.github.com>
2025-12-04 23:04:27 +0530

    Refactor server.js to add login and OTP checks

--- server.js
+++ server.js
@@ -1,20 +1,95 @@
+
 const express = require("express");
 const cors = require("cors");
-
+const db = require("./firebase");
 const app = express();
-app.use(cors());
+const { generateToken } = require("./tokenService");
+const { generateMobileToken } = require("./tokenMobileService");
+app.use(cors({ origin: "http://localhost:4200" }));
 app.use(express.json());
 
-// Root test
 app.get("/", (req, res) => {
-  res.send("Node API working!");
+  res.json({ message: "Node API running!" });
 });
 
-// Example GET API
-app.get("/api/users", (req, res) => {
-  res.json([{ id: 1, name: "John Doe" }, { id: 2, name: "Mary" }]);
+app.post("/api/login", (req, res) => {
+    const { username, email } = req.body;
+
+    // You can also check Firebase DB here if needed
+    const user = {
+        username,
+        email,
+        status: "verified"
+    };
+
+    const token = generateToken(user);
+
+    res.json({
+        status: 200,
+        message: "success",
+        token,
+        user
+    });
 });
 
-// Server Start
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log("Server running on port " + PORT));
+app.post("/api/checkMobileNumber", async (req, res) => {
+  try {
+    const { mobileNumber } = req.body;
+
+    if (!mobileNumber) {
+      return res.status(400).json({ status: 200, message: "failed" });
+    }
+
+    const snapshot = await db
+      .collection("t_realtie_user")
+      .where("mobile_number", "==", Number(mobileNumber))
+      .limit(1)
+      .get();
+
+    if (snapshot.empty) {
+      return res.json({ status: 200, message: "failed" });
+    }
+
+    const user = snapshot.docs[0].data();
+  
+    res.json({ status: 200, message: "success" });
+
+  } catch (error) {
+    res.status(500).json({ status: 400, message: error.message });
+  }
+});
+
+app.post("/api/checkOtp", async (req, res) => {
+  try {
+    const { mobileNumber, mobileOtp } = req.body;
+
+    if (!mobileNumber) {
+      return res.status(400).json({ status: 200, message: "failed" });
+    }
+
+    const snapshot = await db
+      .collection("t_realtie_user")
+      .where("mobile_number", "==", Number(mobileNumber))
+      .where("otp", "==", Number(mobileOtp))
+      .limit(1)
+      .get();
+
+    if (snapshot.empty) {
+      return res.json({ status: 200, message: "failed" });
+    }
+
+    const user = snapshot.docs[0].data();
+    const userDetils = {
+            mobileNumber,
+            status: "verified"
+        };
+    const token = generateMobileToken(userDetils);
+    res.json({ status: 200, message: "success", token });
+
+  } catch (error) {
+    res.status(500).json({ status: 400, message: error.message });
+  }
+});
+
+
+app.listen(3000, () => console.log("Node API started on port 3000"));
