0a16207da59c44d5aae148fda89ad16355558f5a
vadivelczepak <114664327+vadivelczepak@users.noreply.github.com>
2025-12-06 16:37:45 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -9,8 +9,8 @@ admin.initializeApp({
   client_id: process.env.FIREBASE_client_id,
   auth_uri: process.env.FIREBASE_auth_uri,
   token_uri: process.env.FIREBASE_token_uri,
-  auth_provider_cert_url: process.env.FIREBASE_auth_provider_cert_url,
-  client_cert_url: process.env.FIREBASE_client_cert_url,
+  auth_provider_x509_cert_url: process.env.FIREBASE_auth_provider_cert_url,
+  client_x509_cert_url: process.env.FIREBASE_client_cert_url,
   universe_domain: process.env.FIREBASE_universe_domain,
   }),
 });
