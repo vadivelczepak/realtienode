11ec5368127143af8dd619b4d5372a32525700a2
vadivelczepak <114664327+vadivelczepak@users.noreply.github.com>
2025-12-06 11:19:51 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,95 +1,11 @@
-
 const express = require("express");
-const cors = require("cors");
-const db = require("./firebase");
 const app = express();
-const { generateToken } = require("./tokenService");
-const { generateMobileToken } = require("./tokenMobileService");
-app.use(cors({ origin: "http://localhost:4200" }));
-app.use(express.json());
 
 app.get("/", (req, res) => {
-  res.json({ message: "Node API run" });
+  res.send("✅ Test Server is Running on Render!");
 });
 
-app.post("/api/login", (req, res) => {
-    const { username, email } = req.body;
-
-    // You can also check Firebase DB here if needed
-    const user = {
-        username,
-        email,
-        status: "verified"
-    };
-
-    const token = generateToken(user);
-
-    res.json({
-        status: 200,
-        message: "success",
-        token,
-        user
-    });
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log("Server running on port", port);
 });
-
-app.post("/api/checkMobileNumber", async (req, res) => {
-  try {
-    const { mobileNumber } = req.body;
-
-    if (!mobileNumber) {
-      return res.status(400).json({ status: 200, message: "failed" });
-    }
-
-    const snapshot = await db
-      .collection("t_realtie_user")
-      .where("mobile_number", "==", Number(mobileNumber))
-      .limit(1)
-      .get();
-
-    if (snapshot.empty) {
-      return res.json({ status: 200, message: "failed" });
-    }
-
-    const user = snapshot.docs[0].data();
-  
-    res.json({ status: 200, message: "success" });
-
-  } catch (error) {
-    res.status(500).json({ status: 400, message: error.message });
-  }
-});
-
-app.post("/api/checkOtp", async (req, res) => {
-  try {
-    const { mobileNumber, mobileOtp } = req.body;
-
-    if (!mobileNumber) {
-      return res.status(400).json({ status: 200, message: "failed" });
-    }
-
-    const snapshot = await db
-      .collection("t_realtie_user")
-      .where("mobile_number", "==", Number(mobileNumber))
-      .where("otp", "==", Number(mobileOtp))
-      .limit(1)
-      .get();
-
-    if (snapshot.empty) {
-      return res.json({ status: 200, message: "failed" });
-    }
-
-    const user = snapshot.docs[0].data();
-    const userDetils = {
-            mobileNumber,
-            status: "verified"
-        };
-    const token = generateMobileToken(userDetils);
-    res.json({ status: 200, message: "success", token });
-
-  } catch (error) {
-    res.status(500).json({ status: 400, message: error.message });
-  }
-});
-
-
-app.listen(3000, () => console.log("Node API started on port 3000"));
