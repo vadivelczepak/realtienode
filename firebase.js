165a53acf2df524a85cf3a847d59b7646f18ea78
vadivelczepak <114664327+vadivelczepak@users.noreply.github.com>
2025-12-06 10:57:46 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,10 +1,12 @@
 const admin = require("firebase-admin");
 
-// Parse the JSON from environment variable
-const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
-
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount)
+  credential: admin.credential.cert({
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  }),
+  databaseURL: process.env.FIREBASE_DB_URL
 });
 
-const db = admin.firestore(); // Firestore
+module.exports = admin;
