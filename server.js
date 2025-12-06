68456ef6473d00ee8185136f2db53afe7a32ee7e
vadivelczepak <114664327+vadivelczepak@users.noreply.github.com>
2025-12-06 17:41:06 +0530

    Update server.js

--- server.js
+++ server.js
@@ -16,6 +16,32 @@ app.get("/test-write", async (req, res) => {
   res.send("Data written!");
 });
 
+app.post("/api/checkMobileNumber", async (req, res) => {
+  try {
+    const { mobileNumber } = req.body;
+
+    if (!mobileNumber) {
+      return res.status(400).json({ status: 200, message: "failed" });
+    }
+
+    const snapshot = await db
+      .collection("t_realtie_user")
+      .where("mobile_number", "==", Number(mobileNumber))
+      .limit(1)
+      .get();
+
+    if (snapshot.empty) {
+      return res.json({ status: 200, message: "failed" });
+    }
+
+    const user = snapshot.docs[0].data();
+  
+    res.json({ status: 200, message: "success" });
+
+  } catch (error) {
+    res.status(500).json({ status: 400, message: error.message });
+  }
+});
 // Read test data
 app.get("/test-read", async (req, res) => {
   const snap = await db.ref("t_realtie_user").once("value");
