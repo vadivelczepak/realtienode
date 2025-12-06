348d5326459f74e9946fd4a4a9db1b525847d726
vadivelczepak <114664327+vadivelczepak@users.noreply.github.com>
2025-12-06 11:53:35 +0530

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -6,7 +6,7 @@ admin.initializeApp({
     clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
     privateKey: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
   }),
-  databaseURL: process.env.FIREBASE_DB_URL
+  databaseURL: process.env.FIREBASE_DB_URL,
 });
 
 module.exports = admin;
